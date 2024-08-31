4719f5a92b086cb78ef4f0a650182b8d8ddb1166
Ackyla Ferreira <ackyla.dev@gmail.com>
2024-08-31 14:47:19 -0300

    chore(main): deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	base: '/destination-search/',
 	test: {
 		globals: true,
 		environment: 'happy-dom',
