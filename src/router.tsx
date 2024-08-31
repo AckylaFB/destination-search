0a35003a1087b689cf8136647332dd6e3c517062
Ackyla Ferreira <ackyla.dev@gmail.com>
2024-08-31 15:07:47 -0300

    fix(main): fix route

--- src/router.tsx
+++ src/router.tsx
@@ -1,22 +1,25 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Layout, Home, DestinationPage } from '@/pages';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Layout />,
-		children: [
-			{
-				path: '/',
-				element: <Home />,
-				index: true,
-			},
-			{
-				path: '/destination',
-				element: <DestinationPage />,
-			},
-		],
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Layout />,
+			children: [
+				{
+					path: '/',
+					element: <Home />,
+					index: true,
+				},
+				{
+					path: '/destination',
+					element: <DestinationPage />,
+				},
+			],
+		},
+	],
+	{ basename: '/destination-search/' },
+);
 
 export default router;
